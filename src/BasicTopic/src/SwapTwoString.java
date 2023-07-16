03d0998d79dda6c7db8608163ec2f7821edd831d
Md Nurul Amin <nurulaminnion@gmail@gmail.com>
2023-07-16 11:55:02 +0600

    Java started

---
+++ src/BasicTopic/src/SwapTwoString.java
@@ -0,0 +1,15 @@
+
+public class SwapTwoString {
+
+	public static void main(String[] args) {
+		String a="Nion";
+		String b="Ahmed";
+		
+		String temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a+" "+b);
+
+	}
+
+}
